54def3920ddc948cfb2e1cb3536b7214c11a5a32
joaohbbittar <joaohbbitar@gmail.com>
2024-09-24 19:22:16 +0200

    plot results

--- neuromodRNN/src/modRNN/figures/plot_figures.py
+++ neuromodRNN/src/modRNN/figures/plot_figures.py
@@ -39,30 +39,3 @@ def plot_multiruns(experiment_folder, label, metric_file, ax):
 
 
 
-def plot_single_run(experiment_folder, label, metric_file, ax):
-
-
-
-    # Traverse through each run folder inside the experiment folder
-    for run_folder in os.listdir(experiment_folder):
-        run_path = os.path.join(experiment_folder, run_folder)
-        if os.path.isdir(run_path):
-            train_info_folder = os.path.join(run_path, 'train_info')
-                # Look for .pkl files inside the train_info folder
-            if os.path.exists(train_info_folder):
-                file = metric_file
-                file_path = os.path.join(train_info_folder, file) 
-                # Load the .pkl file and extract the loss
-                with open(file_path, 'rb') as f:
-                    result = np.array(pickle.load(f))
-                # Convert list of loss into a numpy array (padding shorter runs with NaN)
-        
-
-
-
-    # Plot the average loss with variance as a shaded area
-    epochs = np.arange(0, result.shape[0] * 25, 25)
-
-    ax.plot(epochs, result, label=label)
-    
-    ax.legend()
